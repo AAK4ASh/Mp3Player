e1eb59072541e83b6b0d86b4113312040cc13694
Aakash <mr.aakash726@gmail.com>
2023-03-23 21:26:56 +0530

    commit2

--- app/src/main/java/com/main/mp3player/MainActivity.kt
+++ app/src/main/java/com/main/mp3player/MainActivity.kt
@@ -16,7 +16,7 @@ import java.io.File
 class MainActivity : AppCompatActivity() {
     private lateinit var binding:ActivityMainBinding
     private lateinit var musicAdapter: MusicAdapter
-    private lateinit var mediaPlayer: MediaPlayer
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
